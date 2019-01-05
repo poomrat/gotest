ed2604c6416474acfc197cd30c095a6ffacdf47f
Poomrat Boonyawong <poomrat.b@linecorp.com>
2019-01-05 11:47:28 +0700

    say two

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,5 +1,8 @@
 package fizzbuzz
 
 func Say(n int) string {
+	if n == 2 {
+		return "2"
+	}
 	return "1"
 }
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -7,10 +7,27 @@ import (
 )
 
 func TestShouldSayOne(t *testing.T) {
+	// Arrange
+
+	// Action
 	result := fizzbuzz.Say(1)
 	expected := "1"
+
+	// Assertion
 	if result != expected {
 		t.Errorf("it should say %q but get %q", expected, result)
 	}
 }
 
+func TestShouldSayTwo(t *testing.T) {
+	// Arrange
+
+	//Action
+	result := fizzbuzz.Say(2)
+
+	// Assertion
+	expected := "2"
+	if result != expected {
+		t.Errorf("it should say %q but get %q", expected, result)
+	}
+}
